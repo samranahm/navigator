d32bdc5da94ec078835c809e7cae3ec9c10a0df1
Samran Ahmed <samranahmed03@gmail.com>
2024-10-30 20:11:17 +0500

    Update project files, modify .gitignore, and remove unused components

---
+++ app/(drawer)/_layout.tsx
@@ -0,0 +1,12 @@
+import { Slot } from "expo-router";
+import { Drawer } from 'expo-router/drawer';
+
+const DrawerLayout = () => {
+  return (
+    <Drawer />
+  )
+   
+   
+};
+
+export default DrawerLayout;
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -30,7 +30,10 @@ export default function RootLayout() {
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="(drawer)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
+
+
       </Stack>
     </ThemeProvider>
   );
